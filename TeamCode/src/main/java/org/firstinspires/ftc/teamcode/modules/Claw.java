0757314552b1201954d9efb4dc62eec2c0c0cd79
VladimirAkhramovich12524 <v.akhramovich@lab244.ru>
2024-10-03 19:29:57 +0300

    добавил Dashboard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Claw.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.modules;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
 public class Claw {
 
     private final Servo ClawServo;
