8fc8362920242a20adadcdb458851f3e57786714
DariaSmirnova12524 <d.smirnova@lab244.ru>
2024-11-06 18:45:50 +0300

    added 2 auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoNaVsyakiiBlizhniii.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoNaVsyakiiBlizhniii.java
@@ -1,4 +1,18 @@
 package org.firstinspires.ftc.teamcode.opmodes.auto;
 
-public class AutoNaVsyakiiBlizhniii {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.modules.DriveTrain;
+
+@Autonomous(name = "Auto Na Vsyakii Sluchai Blizhinii")
+public class AutoNaVsyakiiBlizhniii extends LinearOpMode {
+    DriveTrain base;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        base = new DriveTrain(this);
+        waitForStart();
+        base.driveStraight(.25, 70);
+    }
 }
