060ee1248a3e20ae9b40a3932c6c9e656684e906
Vladimir Akhramovich <v.akhramovich@lab244.ru>
2024-12-19 19:19:47 +0300

    Добавил управление для клешни для образца

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Claw.java
@@ -7,41 +7,59 @@ import com.qualcomm.robotcore.hardware.Servo;
 @Config
 public class Claw {
 
-    private final Servo clawServo;
+    private final Servo clawServoShoulder;
+    private final Servo clawServoLift;
     private final LinearOpMode opMode;
 
     public static double CLAW_CLOSE = 0.735;
     public static double CLAW_HALF_OPEN = 0.6;
     public static double CLAW_OPEN = 0.5;
 
-    public boolean stateOpen;
+    public boolean stateOpenShoulder;
+    public boolean stateOpenLift;
 
 
     public Claw(LinearOpMode opMode) {
         this.opMode = opMode;
-        clawServo = opMode.hardwareMap.servo.get("ClawServo");
+        clawServoShoulder = opMode.hardwareMap.servo.get("ClawServoShoulder");
+        clawServoLift = opMode.hardwareMap.servo.get("ClawServoLift");
 
     }
 
-    public void close() {
-        clawServo.setPosition(CLAW_CLOSE);
-        stateOpen = false;
+    public void halfOpenSh() {
+        clawServoShoulder.setPosition(CLAW_HALF_OPEN);
+    }
+    public void halfOpenLift() {
+        clawServoLift.setPosition(CLAW_HALF_OPEN);
     }
 
-    public void halfOpen() {
-        clawServo.setPosition(CLAW_HALF_OPEN);
-    }
-    public void open() {
-        clawServo.setPosition(CLAW_OPEN);
-        stateOpen = true;
-    }
-    public void switchPosition() {
-        if (!stateOpen) {
-            clawServo.setPosition(CLAW_OPEN);
-            stateOpen = true;
+    public void switchPositionShoulder() {
+        if (!stateOpenShoulder) {
+            clawServoShoulder.setPosition(CLAW_OPEN);
+            stateOpenShoulder = true;
         } else {
-            clawServo.setPosition(CLAW_CLOSE);
-            stateOpen = false;
+            clawServoShoulder.setPosition(CLAW_CLOSE);
+            stateOpenShoulder = false;
         }
+    }
+    public void switchPositionLift() {
+        if (!stateOpenLift) {
+            clawServoLift.setPosition(CLAW_OPEN);
+            stateOpenLift = true;
+        } else {
+            clawServoLift.setPosition(CLAW_CLOSE);
+            stateOpenLift = false;
+        }
+    }
+
+
+
+    public void closeSh() {
+        clawServoShoulder.setPosition(CLAW_CLOSE);
+        stateOpenShoulder = false;
+    }
+    public void openSh() {
+        clawServoShoulder.setPosition(CLAW_OPEN);
+        stateOpenShoulder = true;
     }
 }
