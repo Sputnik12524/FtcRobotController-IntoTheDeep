b3786469e977f1d4ed1757d6cd60c1344c7795c1
DariaSmirnova12524 <d.smirnova@lab244.ru>
2025-03-05 19:59:22 +0300

    tuned rr 1.0 (but test auto not works))

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -57,8 +57,8 @@ public final class MecanumDrive {
 
         // path controller gains
         public double axialGain = 16;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double lateralGain = 1;
+        public double headingGain = 2; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/TestAuto.java
@@ -3,16 +3,25 @@ package org.firstinspires.ftc.teamcode.roadrunner;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.modules.DriveTrainMecanum;
 
+@Autonomous(name = "test", group = "Robot")
 public class TestAuto extends LinearOpMode {
     @Override
     public void runOpMode() {
-        Pose2d beginPose = new Pose2d(0,0,0);
-        DriveTrainMecanum driveTrainMecanum = new DriveTrainMecanum(hardwareMap,beginPose);
-        TrajectoryActionBuilder traj = driveTrainMecanum.actionBuilder(beginPose)
-                .turn(Math.toRadians(90)).lineToX(52);
+        Pose2d beginPose = new Pose2d(0, 0, 0);
+        MecanumDrive driveTrainMecanum = new MecanumDrive(hardwareMap, beginPose);
+        waitForStart();
+        if (isStopRequested()) return;
+        Actions.runBlocking(
+                driveTrainMecanum.actionBuilder(new Pose2d(0, 0, 0))
+                        .turn(Math.PI / 2)
+                        .lineToY(15)
+                        .build());
+
     }
 }
