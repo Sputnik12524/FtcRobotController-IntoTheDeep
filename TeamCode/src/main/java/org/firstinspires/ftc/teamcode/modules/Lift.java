b0ca332596b75103c119a8204592d6548816fa92
Vladimir Akhramovich <v.akhramovich@lab244.ru>
2025-03-18 15:26:52 +0300

    Одну позицию подъемника изменил

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Lift.java
@@ -35,7 +35,7 @@ public class Lift {
     public static double POS_SIDE = -4.5; // Берем с борта // -4 TRUE
      public static double POS_LOW_SPECIMEN_BEFORE = -30; // Целимся для установки // -15 TRUE
     public static double POS_LOW_SPECIMEN_AFTER = -2; // Устанавливаем образец
-    public static double POS_HIGH_SPECIMEN_BEFORE = -30; // Целимся для установки
+    public static double POS_HIGH_SPECIMEN_BEFORE = -35; // Целимся для установки
     public static double POS_HIGH_SPECIMEN_AFTER = -25; // Устанавливаем образец
 
 
